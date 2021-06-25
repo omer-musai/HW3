87355089cfa8039e2cea9ac9b9e869fd38f7f5c2
Shay <ShayHacohen14@gmail.com>
2021-06-25 19:38:52 +0300

    Adding an initial version of check_strings.

---
+++ check_strings_prototype.py
@@ -0,0 +1,15 @@
+s1 = 'abcdddea'
+
+
+def make_histogram(string: str) -> dict:
+    string = string.lower()
+    letters = [chr(letter) for letter in range(ord('a'), ord('z') + 1)]
+    letter_count = [string.count(letter) for letter in letters]
+    return dict(zip(letters, letter_count))
+
+def check_strings(s1: str, s2: str) -> bool:
+    s1_histogram = make_histogram(s1)
+    s2_histogram = make_histogram(s2)
+    letters = [chr(letter) for letter in range(ord('a'), ord('z') + 1)]
+    
+    return all([s1_histogram[letter] <= s2_histogram[letter] for letter in letters])
