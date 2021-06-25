5bd5dd85039af3e73b3a3ab3042d5bc10b15b483
Shay <ShayHacohen14@gmail.com>
2021-06-25 19:14:52 +0300

    A bunch of fixes.

--- gradesCalc.py
+++ gradesCalc.py
@@ -7,48 +7,47 @@ LOWEST_AVERAGE_GRADE = 50
 HIGHEST_AVERAGE_GRADE = 100
 
 
-def final_grade(input_path: str, output_path: str) -> int:
-    if (type(input_path) != str or type(output_path) != str):
-        raise TypeError('Invalid type.')
-    
-    data_dict = {}
+def final_grade(input_path: str, output_path: str):
+    if type(input_path) != str or type(output_path) != str:
+        raise TypeError('File paths must be strings.')
+
+    data = {}
     input_file = open(input_path, 'r')
 
     for line in input_file:
         if not is_legal_line(line):
             continue
-        
-        current_data = parse_line(str)
-        data_dict[current_data[0]] = current_data[1:]
-    
-    close(input_file)
+
+        current_data = parse_line(line)
+        data[current_data[0]] = current_data[1:]
+
+    input_file.close()
 
     output_file = open(output_path, 'w')
-    
-    sorted_keys = list(data_dict.keys())
+
+    sorted_keys = list(data.keys())
     sorted_keys.sort()
     for key in sorted_keys:
-        homework_avg, final_grade = data_dict[key]
-        output_file.write(key + ', ' + str(homework_avg) + ', ' + str(final_grade) + '\n')
-    
-    close(output_file)
-        
+        homework_avg, grade = data[key]
+        output_file.write(', '.join((key, str(homework_avg), str(grade))) + '\n')
 
-def calculate_final_grade(ID: str, homework_avg: int):
-    #TODO: Ask in Piazza if we need to make these poor 2s into constants.
-    return int((int(ID[-2:]) + homework_avg) / 2)
+    output_file.close()
 
 
-#Returns a tuple of the form (ID: str, homework_avg: int, final_grade: int)
+def calculate_final_grade(id_number: str, homework_avg: int):
+    # TODO: Ask in Piazza if we need to make these poor 2s into constants.
+    return int((int(id_number[-2:]) + homework_avg) / 2)
+
+
+# Returns a tuple of the form (ID: str, homework_avg: int, final_grade: int)
 def parse_line(line: str):
     line = line.split(',')
 
     id_number = line[ID_INDEX]
     homework_avg = line[GRADE_INDEX]
-    grade = calculate_final_grade(ID, homework_avg)
+    grade = calculate_final_grade(id_number, int(homework_avg))
 
-    return (id_number, homework_avg, grade)
-    
+    return id_number, homework_avg, grade
 
 
 def is_legal_line(line: str):
@@ -56,30 +55,19 @@ def is_legal_line(line: str):
 
     legal_id = is_legal_id(line[ID_INDEX])
     legal_name = is_legal_name(line[NAME_INDEX])
-    legal_semester = line[SEMESTER_INDEX] >= 1
-    legal_average_grade = LOWEST_AVERAGE_GRADE < line[GRADE_INDEX] =< HIGHEST_AVERAGE_GRADE
-    
+    legal_semester = int(line[SEMESTER_INDEX]) >= 1
+    legal_average_grade = LOWEST_AVERAGE_GRADE < int(line[GRADE_INDEX]) <= HIGHEST_AVERAGE_GRADE
+
     return legal_id and legal_name and legal_semester and legal_average_grade
-    
-    
-def is_legal_id(idToCheck: str):
-    if type(idToCheck) != str:
-        return len(idToCheck) == ID_LENGTH and idToCheck[0] != '0' and idToCheck.isdigit()
-    
+
+
+def is_legal_id(id_to_check: str):
+    if type(id_to_check) != str:
+        return len(id_to_check) == ID_LENGTH and id_to_check[0] != '0' and id_to_check.isdigit()
+
     return False
+
 
 def is_legal_name(name: str):
     name = [word for word in name.split(' ') if len(word) > 0]
-    
-    for word in name:
-        if(not word.isalpha()):
-            return False
-
-    return True
-
-
-
-
-
-
-    
+    return len([word for word in name if not word.isalpha()]) == 0
